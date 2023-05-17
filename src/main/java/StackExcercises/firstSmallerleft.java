93c13d0cd8e798d71aaed84f735138249e611acf
pandeyank <pandeyankit.mit@gmail.com>
2023-05-17 23:52:48 +0530

    adding new files in Stack

---
+++ src/main/java/StackExcercises/firstSmallerleft.java
@@ -0,0 +1,28 @@
+package StackExcercises;
+
+public class firstSmaller {
+    public static void main(String[] args) {
+        int[] arr={4,5,2,10,3,12};
+        int n=arr.length;
+        int [] res=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            res[i]=-1;
+        }
+        for(int i=1;i<n;i++)
+        {
+            for(int j=i-1;j>=0;j--)
+            {
+                if(arr[j]<arr[i])
+                {
+                    res[i]=arr[j];
+                    break;
+                }
+            }
+        }
+        for(int i=0;i<n;i++)
+        {
+            System.out.print(res[i] +" ");
+        }
+    }
+}
---
+++ src/main/java/StackExcercises/smallerIndexRight.java
@@ -0,0 +1,2 @@
+package StackExcercises;public class smallerIndexRight {
+}
